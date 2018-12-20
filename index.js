f7cfa615b97e22817c228e0f8f549a18b4c6db29
Richard Schaubeck <richard.schaubeck@gmail.com>
2018-12-20 18:21:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 $(document).ready(function (){
 });
+
+function searchRepositories() {
+  let keyword = $('#searchTerms').value;
+  let uri = 'https://api.github.com/search/repositories?q=' + keyword;
+  $.get(uri, function(response) {
+    const src = $('#repository-template').innerHTML;
+    const template = Handlebars.compile(src);
+    console.log(response);
+  })
+}
